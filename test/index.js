a19215e1227aa72a43387e66d436e9f1effcab21
berwin <berwin1995@qq.com>
2017-06-12 18:33:48 +0800

    add test

--- test/index.js
+++ test/index.js
@@ -1,13 +1,61 @@
 const test = require('ava')
+const path = require('path')
+const inquirer = require('inquirer')
+const fs = require('fs')
+const SpeikeInit = require('../lib/init.js')
+const utils = require('../lib/utils.js')
+const cacheTemplatePath = path.join(__dirname, '.speike-templates')
+const targetPath = path.join(__dirname, 'speike-cli-unit-test')
+
+const answers = {
+  name: 'speike-cli-test',
+  author: 'Berwin <liubowen.niubi@gmail.com>',
+  description: 'speike-cli unit test'
+}
 
 test.before(() => {
-  console.log('test before')
+  inquirer.prompt = (questions) => {
+    const _answers = {}
+    for (var i = 0; i < questions.length; i++) {
+      const key = questions[i].name
+      _answers[key] = answers[key]
+    }
+    return Promise.resolve(_answers)
+  }
 })
 
-test('should xxx', t => {
-  t.pass()
+test.cb('should generation speike-cli-unit-test project', t => {
+  const init = new SpeikeInit({
+    template: 'speike-template-haotech',
+    name: 'speike-cli-unit-test',
+    cacheTemplatePath,
+    targetPath,
+    clone: false
+  })
+
+  init.run()
+
+  const timer = setInterval(() => {
+    if (utils.isExist(targetPath)) {
+      clearInterval(timer)
+      t.pass()
+      t.end()
+    }
+  }, 1000)
+})
+
+test.cb('project content be equal to answers', t => {
+  fs.readFile(path.join(targetPath, 'package.json'), 'utf8', (err, data) => {
+    if (err) throw err
+    const json = JSON.parse(data)
+    t.is(json.name, answers.name)
+    t.is(json.description, answers.description)
+    t.is(json.author, answers.author)
+    t.end()
+  })
 })
 
 test.after(t => {
-  console.log('test after')
+  utils.rmdir(cacheTemplatePath)
+    .then(() => utils.rmdir(targetPath))
 })
